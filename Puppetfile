2a43af5c339c77a42c558063bbce7489e84fbad4
Skablam <stainlesssteelmatt@gmail.com>
2015-11-03 11:34:07 +0000

    added new api_skeleton to mods

--- Puppetfile
+++ Puppetfile
@@ -4,6 +4,6 @@ mod 'LandRegistry/standard_env',
     git: 'git://github.com/LandRegistry/standard-env'
 
 mod 'LandRegistry/api_skeleton',
-        git: 'git://github.com/LandRegistry/charges-api-skeleton',
+        git: 'git://github.com/LandRegistry/dm-api-skeleton',
         ref: 'master',
         path: 'puppet/api_skeleton'
--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ Exec { path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
 # Default file permissions to root:root
 File { owner => 'root', group => 'root', }
 
-# Explicitly set 'allow virtual packages to false' in order to surpres error
+# Explicitly set 'allow virtual packages to false' in order to suppress error
 # message on CentOS.
 if versioncmp($::puppetversion,'3.6.1') >= 0 {
 
@@ -18,10 +18,9 @@ if versioncmp($::puppetversion,'3.6.1') >= 0 {
 node default {
   require ::standard_env
   include ::standard_env::tools::cucumber
+  include ::api_skeleton
 
   service { 'firewalld':
     ensure => 'stopped',
-
-    include::api_skeleton
   }
 }
